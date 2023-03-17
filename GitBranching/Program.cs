0c29a87d26cc079eb59605e3236b32f62e19939f
Carolina Bastos <carol1999b@gmail.com>
2023-03-17 10:48:53 +0000

    Creat a new branch and change WriteLine

--- GitBranching/Program.cs
+++ GitBranching/Program.cs
@@ -6,8 +6,7 @@ namespace GitBranching
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello again!");
+            Console.WriteLine("Bye");
         }
     }
 }
