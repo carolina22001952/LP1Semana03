99b96da4dbb17edbf27c1eaa49247919bff67c2f
Carolina Bastos <carol1999b@gmail.com>
2023-03-17 10:17:05 +0000

    Add new project ChangeString

---
+++ ChangeString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userString;
+            char userChar;
+
+            Console.WriteLine("String: ");
+            userString = Console.ReadLine();
+
+            Console.WriteLine("Char: ");
+            userChar = char.Parse(Console.ReadLine());
+
+            foreach (char c in userString)
+            {
+                if (c != userChar)
+                {
+                    Console.WriteLine(c);
+                }
+            }
+        }
+    }
+}
