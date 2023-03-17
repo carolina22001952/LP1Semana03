24705c85b86779d6af67e26ab4980699a0a9691d
Carolina Bastos <carol1999b@gmail.com>
2023-03-17 11:39:01 +0000

    Add project PlayerPerks

---
+++ PlayerPerks/Perks.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+namespace PlayerPerks
+{
+    [Flags]
+    public enum Perks
+    {
+        WaterBreathing = 1 << 0,
+        Stealth = 1 << 1,
+        AutoHeal = 1 << 2,
+        DoubleJump = 1 << 3    
+    }
+}
---
+++ PlayerPerks/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace PlayerPerks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+                return;
+
+            Perks perks = 0;
+
+            foreach (char c in args[0])
+            {
+                switch (c)
+                {
+                    case 'w':
+                        perks ^= Perks.WaterBreathing;
+                        break;
+                    case 'a':
+                        perks ^= Perks.AutoHeal;
+                        break;
+                    case 's':
+                        perks ^= Perks.Stealth;
+                        break;
+                    case 'd':
+                        perks ^= Perks.DoubleJump;
+                        break;
+                    default:
+                        Console.WriteLine("Unknown perk!");
+                        return;
+                }
+            }
+
+            if (perks == 0)
+            {
+                Console.WriteLine("No perks at all!");
+                return;
+            }
+
+            Console.WriteLine(perks);
+
+            if ((perks & (Perks.Stealth | Perks.DoubleJump)) == (Perks.Stealth | Perks.DoubleJump))
+                Console.WriteLine("Silent jumper!");
+
+            if ((perks & Perks.AutoHeal) != Perks.AutoHeal)
+                Console.WriteLine("Not gonna make it!");
+        }
+    }
+}
